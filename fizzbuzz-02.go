1eab9452440b908d227496d80ae346dce05883a6
mi-bear <kumajet@gmail.com>
2017-07-12 15:09:41 +0900

    fizzbuzz-02 add

--- fizzbuzz-02.go
+++ fizzbuzz-02.go
@@ -1,5 +1,38 @@
 package main
 
-func main() {
+import "fmt"
 
+var (
+	max  = 100
+	fizz = 3
+	buzz = 5
+)
+
+func main() {
+	i := 1
+	f := fizz
+	b := buzz
+
+	for i <= max {
+		var fb bool
+
+		if i == f {
+			f += fizz
+			fb = true
+			fmt.Printf("%s", "Fizz")
+		}
+
+		if i == b {
+			b += buzz
+			fb = true
+			fmt.Printf("%s", "Buzz")
+		}
+
+		if !fb {
+			fmt.Printf("%d", i)
+		}
+
+		fmt.Printf("%s", "\n")
+		i++
+	}
 }
